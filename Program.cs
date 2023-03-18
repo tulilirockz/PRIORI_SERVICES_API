c599b6e302d7f7aa874531a22362ebc5fb68b2c0
tulimenot <tulilirockz.pub+git@gmail.com>
2023-03-18 18:42:06 -0300

    Add dependencies and disable swagger for managing through insomnia (better testing)

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<PrioriDbContext>(opt => 
+    opt.UseSqlServer(
+        "Server=192.168.15.14,1433;DataBase=Priori;user id=sa;password=_ScoobyDooby23;Encrypt=True;TrustServerCertificate=True"
+    )
+);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
