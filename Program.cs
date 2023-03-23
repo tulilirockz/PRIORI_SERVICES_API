4aa396bc80e02e3b3348544e20b0c253000f86e2
tulimenot <tulilirockz.pub+git@gmail.com>
2023-03-23 17:38:08 -0300

    Use appsettings to gather database information instead of hardcoded values

--- Program.cs
+++ Program.cs
@@ -3,16 +3,16 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<PrioriDbContext>(opt => 
+
+builder.Services.AddDbContext<PrioriDbContext>(opt =>
     opt.UseSqlServer(
-        "Server=192.168.15.14,1433;DataBase=Priori;user id=sa;password=_ScoobyDooby23;Encrypt=True;TrustServerCertificate=True"
+        builder.Configuration["ConnectionString"]
     )
 );
+
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
