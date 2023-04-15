88a3b8302133c147fdac4c84b33bdfd911d8f686
Tulili Blossom <tulilirockz.pub+git@gmail.com>
2023-04-15 01:08:55 +0000

    refactor: mover jwt p/ repositories

---
+++ Repository/JwtHandler.cs
@@ -0,0 +1,22 @@
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace PRIORI_SERVICES_API.Repository;
+public static class JwtHandler
+{
+    public static JwtSecurityToken GenerateJWTToken(IConfiguration configuration_context, List<Claim> claims, string jwt_key_attribute = "JWTKey")
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            configuration_context.GetSection("JWTKey").Value!));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        return new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: creds
+            );
+    }
+}
