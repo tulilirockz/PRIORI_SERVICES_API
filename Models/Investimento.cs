dd34bcdfcb578106a005f48b43269523e14dba2f
Tulili Blossom <tulilirockz.pub+git@gmail.com>
2023-04-15 14:54:12 +0000

    feat: maior versatilidade do mapeamento de entidades

--- Models/Investimento.cs
+++ Models/Investimento.cs
@@ -1,34 +1,53 @@
 using PRIORI_SERVICES_API.Models.Dbos;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
-namespace PRIORI_SERVICES_API.Models
+namespace PRIORI_SERVICES_API.Models;
+
+[PrimaryKey(nameof(id_investimento))]
+[Table("tblInvestimentos")]
+public class Investimento
 {
-    public class Investimento
+    public InvestimentoDbo toDbo(Investimento inv)
     {
-        public InvestimentoDbo toDbo(Investimento inv)
+        return new InvestimentoDbo
         {
-            return new InvestimentoDbo
-            {
-                id_investimento = inv.id_investimento,
-                id_riscoInvestimento = inv.id_riscoInvestimento,
-                nome = inv.nome,
-                tipo_investimento = inv.tipo_investimento,
-                rentabilidade_fixa = inv.rentabilidade_fixa,
-                rentabilidade_variavel = inv.rentabilidade_variavel,
-                data_atualizacao = inv.data_atualizacao,
-                vencimento = inv.vencimento,
-                valor_minimo = inv.valor_minimo,
-                tempo_minimo = inv.tempo_minimo,
-            };
-        }
-        public int id_investimento { get; set; }
-        public Decimal id_riscoInvestimento { get; set; }
-        public string? nome { get; set; }
-        public string? tipo_investimento { get; set; }
-        public Decimal rentabilidade_fixa { get; set; }
-        public Decimal rentabilidade_variavel { get; set; }
-        public DateTime data_atualizacao { get; set; }
-        public DateTime vencimento { get; set; }
-        public Decimal valor_minimo { get; set; }
-        public Decimal tempo_minimo { get; set; }
+            id_investimento = inv.id_investimento,
+            id_riscoInvestimento = inv.id_riscoInvestimento,
+            nome = inv.nome,
+            tipo_investimento = inv.tipo_investimento,
+            rentabilidade_fixa = inv.rentabilidade_fixa,
+            rentabilidade_variavel = inv.rentabilidade_variavel,
+            data_atualizacao = inv.data_atualizacao,
+            vencimento = inv.vencimento,
+            valor_minimo = inv.valor_minimo,
+            tempo_minimo = inv.tempo_minimo,
+        };
     }
+    [Column(TypeName = "int")]
+    public int id_investimento { get; set; }
+
+    [Column(TypeName = "numeric")]
+    public Decimal id_riscoInvestimento { get; set; }
+
+    [Column(TypeName = "varchar(40)")]
+    public string? nome { get; set; }
+
+    [Column(TypeName = "varchar(5)")]
+    public string? tipo_investimento { get; set; }
+
+    [Column(TypeName = "numeric(8,4)")]
+    public Decimal rentabilidade_fixa { get; set; }
+    [Column(TypeName = "numeric(8,2)")]
+    public Decimal rentabilidade_variavel { get; set; }
+    [Column(TypeName = "datetime")]
+    public DateTime data_atualizacao { get; set; }
+    [Column(TypeName = "date")]
+    public DateTime vencimento { get; set; }
+    [Column(TypeName = "numeric(8,2)")]
+    public Decimal valor_minimo { get; set; }
+    [Column(TypeName = "numeric(3)")]
+    public Decimal tempo_minimo { get; set; }
 }
+
--- Models/PostBlog.cs
+++ Models/PostBlog.cs
@@ -1,25 +1,37 @@
 using PRIORI_SERVICES_API.Models.Dbos;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace PRIORI_SERVICES_API.Model;
 
+[Table("tblPostBlog")]
+[PrimaryKey(nameof(id_post))]
 public class PostBlog
 {
-    public PostBlogDbo toDBO(PostBlog hi)
+    public PostBlogDBO toDBO(PostBlog DBO)
     {
-        return new PostBlogDbo
+        return new PostBlogDBO
         {
-            id_autor = hi.id_autor,
-            id_categoria = hi.id_categoria,
-            titulo = hi.titulo,
-            conteudo = hi.conteudo,
-            descricao = hi.descricao
+            id_autor = DBO.id_autor,
+            id_categoria = DBO.id_categoria,
+            titulo = DBO.titulo,
+            conteudo = DBO.conteudo,
+            descricao = DBO.descricao
         };
     }
+    [Column(TypeName = "int")]
     public int id_post { get; set; }
+    [Column(TypeName = "int")]
     public int id_autor { get; set; }
+    [Column(TypeName = "int")]
     public int id_categoria { get; set; }
+    [Column(TypeName = "datetime")]
     public DateTime data_criacao { get; set; }
+    [Column(TypeName = "varchar(100)")]
     public string? titulo { get; set; } = null;
+    [Column(TypeName = "varchar(200)")]
     public string? descricao { get; set; } = null;
+    [Column(TypeName = "varchar(2000)")]
     public string? conteudo { get; set; } = null;
 }
