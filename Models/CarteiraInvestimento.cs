4d73d4f50ecc73159df1828cac2efe57508b6caf
Gabriel <97401164+Gabriel-Almeida00@users.noreply.github.com>
2023-04-15 00:16:48 -0300

    fix : tirando as propriedades de navegação (#10)

--- Models/CarteiraInvestimento.cs
+++ Models/CarteiraInvestimento.cs
@@ -9,6 +9,37 @@ public class CarteiraInvestimento
     {
         return new CarteiraInvestimentoDbo
         {
+<<<<<<< HEAD
+            return new CarteiraInvestimentoDbo
+            {
+                id_efetuacao = ct.id_efetuacao,
+                id_cliente_carteira = ct.id_cliente_carteira,
+                id_investimento = ct.id_investimento,
+                rentabilidade_fixa = ct.rentabilidade_fixa,
+                rentabilidade_variavel = ct.rentabilidade_variavel,
+                valor_aplicado = ct.valor_aplicado,
+                data_encerramento = ct.data_encerramento,
+                data_efetuacao = ct.data_efetuacao,
+                status = ct.status,
+                saldo = ct.saldo
+            };
+        }
+        public int id_efetuacao { get; set; }
+
+        
+        public int id_cliente_carteira { get; set; }
+
+       
+        public int id_investimento { get; set; }
+
+        public Decimal rentabilidade_fixa { get; set; }
+        public Decimal rentabilidade_variavel { get; set; }
+        public DateTime data_efetuacao { get; set; }
+        public Decimal valor_aplicado { get; set; }
+        public DateTime ?data_encerramento { get; set; }
+        public string ?status { get; set; }
+        public Decimal saldo { get; set; }
+=======
             id_efetuacao = ct.id_efetuacao,
             id_cliente_carteira = ct.id_cliente_carteira,
             id_investimento = ct.id_investimento,
@@ -20,6 +51,7 @@ public class CarteiraInvestimento
             status = ct.status,
             saldo = ct.saldo
         };
+>>>>>>> 17ba24e6f57a99741d77f6b1890ce5cbf605d512
     }
     public int id_efetuacao { get; set; }
 
