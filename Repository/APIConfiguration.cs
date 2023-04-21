01316a0540a4b9de93597290fead7147ba188787
Tulili <tulilirockz.pub+git@gmail.com>
2023-04-21 00:41:18 +0000

    fix: especificar PRIORI_DATABASE_USER na config

--- Repository/APIConfiguration.cs
+++ Repository/APIConfiguration.cs
@@ -6,6 +6,6 @@ public static class APIConfiguration
     public static string PRIORI_DATABASE_PORT { get; set; } = Environment.GetEnvironmentVariable("PRIORI_DATABASE_PORT") ?? "1433";
     public static string PRIORI_DATABASE_IP { get; set; } = Environment.GetEnvironmentVariable("PRIORI_DATABASE_IP") ?? "localhost";
     public static string PRIORI_DATABASE_NAME { get; set; } = Environment.GetEnvironmentVariable("PRIORI_DATABASE_NAME") ?? "Priori";
-    public static string? PRIORI_DATABASE { get; set; } = Environment.GetEnvironmentVariable("PRIORI_DATABASE_USER");
+    public static string? PRIORI_DATABASE_USER { get; set; } = Environment.GetEnvironmentVariable("PRIORI_DATABASE_USER") ?? "sa";
     public static string? PRIORI_DATABASE_PASSWORD { get; set; } = Environment.GetEnvironmentVariable("PRIORI_DATABASE_PASSWORD");
 }
