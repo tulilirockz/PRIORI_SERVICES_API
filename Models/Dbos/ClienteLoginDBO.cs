2e9af208826d482e6bffdca40ea242cb9ec63343
Tulili <tulilirockz.pub+git@gmail.com>
2023-04-29 01:08:07 +0000

    fix: melhorar segurança com post request no ClienteController

---
+++ Models/Dbos/ClienteLoginDBO.cs
@@ -0,0 +1,7 @@
+namespace PRIORI_SERVICES_API.Models.Dbos;
+
+public sealed class ClienteLoginDBO
+{
+    public string email { get; set;} = String.Empty;
+    public string senha { get; set;} = String.Empty;
+}
