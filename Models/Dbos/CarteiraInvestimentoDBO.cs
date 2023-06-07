861c66f661ce0caaa6753c8bad69cdbcf383292d
Tulili <tulilirockz.pub+git@gmail.com>
2023-06-07 00:22:29 -0300

    fix: set nullable data_encerramento na carteiraDBO

--- Models/Dbos/CarteiraInvestimentoDBO.cs
+++ Models/Dbos/CarteiraInvestimentoDBO.cs
@@ -8,7 +8,7 @@ public class CarteiraInvestimentoDBO
     public Decimal rentabilidade_fixa { get; set; }
     public Decimal rentabilidade_variavel { get; set; }
     public Decimal valor_aplicado { get; set; }
-    public DateTime data_encerramento { get; set; }
+    public DateTime? data_encerramento { get; set; }
     public string? status { get; set; }
     public Decimal saldo { get; set; }
 }
